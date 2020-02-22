7bf540335e9140b113713bbf7edb2b91629bffb5
Tony Nguyen <anhhao1999@gmail.com>
2020-02-22 16:10:24 -0500

    specify sklearn version

--- entity_extraction.py
+++ entity_extraction.py
@@ -3,15 +3,11 @@ from app_utils import get_inputs
 from flask import jsonify
 
 nlp = spacy.load("en_core_web_sm")
-nlp2 = spacy.load("entity_recognizer")
-
 
 def get_entity():
     inputs = get_inputs()
     sentence = inputs["sentence"]
     doc = nlp(sentence)
-    if not len(doc.ents):
-        doc = nlp2(sentence)
 
     response = {
         "entities": list(doc.ents)
