60e5923661c094e06d58d92173cb19a29f60ced2
Tony Nguyen <anhhao1999@gmail.com>
2020-02-23 02:03:05 -0500

    fix vector rep to use also use encorewebsm as module

--- vector_rep.py
+++ vector_rep.py
@@ -1,11 +1,12 @@
 import json
 import numpy as np
 import spacy
+import en_core_web_sm
 
 
 indices = json.load(open("indices.json"))
 vecs = np.load("word2vec.npy")
-nlp = spacy.load('en_core_web_sm')
+nlp = en_core_web_sm.load()
 
 
 def sentence2vec(sentence):
